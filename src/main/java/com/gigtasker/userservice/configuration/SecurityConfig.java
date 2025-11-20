fae7c6a9b219abaf7cbcead433b1a97cafff8227
Utkarsh Lohani <utkarshlohani88380@gmail.com>
2025-11-20 21:27:57 +0530

    - Added springdoc-openapi-starter-webmvc-ui for Swagger - Removed SecurityConfig as it is now imported by reading gigtasker common SecurityConfig

--- src/main/java/com/gigtasker/userservice/configuration/SecurityConfig.java
+++ src/main/java/com/gigtasker/userservice/configuration/SecurityConfig.java
@@ -1,42 +1,10 @@
 package com.gigtasker.userservice.configuration;
 
 import org.gigtasker.gigtaskercommon.security.GigTaskerSecurity;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
 @Import(GigTaskerSecurity.class)
-@EnableMethodSecurity
 public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .cors(withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize -> authorize
-                        // Permitting Health Checks
-                        .requestMatchers("/actuator/health/**").permitAll()
-                        // Global Rules
-                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        .anyRequest().authenticated()
-                ).oauth2ResourceServer(oauth2 -> oauth2
-                        // This automatically picks up the 'jwtAuthenticationConverter'
-                        // from GigTaskerSecurity.java
-                        .jwt(withDefaults())
-                );
-
-        return http.build();
-    }
 }
